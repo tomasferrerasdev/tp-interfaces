1b75f24f3ea43bc3ebae0a8828f2372990818e41
Ivan Peuscovich <ivi_peusco2001@hotmail.com>
2022-09-29 13:56:21 -0300

    carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,28 @@ setInterval(() => {
   progressBar.style.setProperty('--width', width + 0.1);
   if (width === 100) loaderContainer.classList.add('remove');
 }, 4);
+
+
+var slideIndex = 1;
+      showSlides(slideIndex);
+      function plusSlides(n) {
+         showSlides(slideIndex += n);
+      }
+      function currentSlide(n) {
+         showSlides(slideIndex = n);
+      }
+      function showSlides(n) {
+         var i;
+         var slides = document.getElementsByClassName("mySlides");
+         var dots = document.getElementsByClassName("dot");
+         if (n > slides.length) {slideIndex = 1}
+         if (n < 1) {slideIndex = slides.length}
+         for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+         }
+         for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+         }
+         slides[slideIndex-1].style.display = "block";
+         dots[slideIndex-1].className += " active";
+      }
