19607676a53af7a33c697655b5d53dea66ecf399
Tomas Ferreras <hellotomasdev@gmail.com>
2022-10-09 17:48:37 -0300

    push tp2

---
+++ TP2/assets/js/main.js
@@ -0,0 +1,80 @@
+// MENU
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+const slider = document.querySelectorAll('.slider-move');
+
+let sliderGrabbed = false;
+
+slider.forEach((element) => {
+  element.addEventListener('mousedown', (e) => {
+    sliderGrabbed = true;
+    element.style.cursor = 'grabbing';
+  });
+});
+
+slider.forEach((element) => {
+  element.addEventListener('mouseup', (e) => {
+    sliderGrabbed = false;
+    element.style.cursor = 'grab';
+  });
+});
+
+slider.forEach((element) => {
+  element.addEventListener('mouseleave', (e) => {
+    sliderGrabbed = false;
+  });
+});
+
+slider.forEach((element) => {
+  element.addEventListener('mousemove', (e) => {
+    if (sliderGrabbed) {
+      element.parentElement.scrollLeft -= e.movementX;
+    }
+  });
+});
+
+slider.forEach((element) => {
+  element.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    element.parentElement.scrollLeft += e.deltaY;
+  });
+});
+
+const body = document.getElementById('body');
+const loader = document.getElementById('loader');
+
+body.style.overflow = 'hidden';
+setTimeout(() => {
+  body.style.overflow = 'visible';
+  loader.style.display = 'none';
+}, 5000);
+
+// function getScrollPercentage() {
+//   return (
+//     (slider.parentElement.scrollLeft /
+//       (slider.parentElement.scrollWidth - slider.parentElement.clientWidth)) *
+//     100
+//   );
+// }
